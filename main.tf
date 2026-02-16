fe85eb6f91697f4b116a891de6d656d355bb1656
Franklyn Duran <frdsoto.25@gmail.com>
2026-02-16 16:03:53 -0400

    Preparacio para subir a github

--- main.tf
+++ main.tf
@@ -5,4 +5,15 @@ provider "aws" {
 resource "aws_instance" "nginx-server"{
     ami             = "ami-05efc83cb5512477c"
     instance_type   = "t3.micro"
+<<<<<<< HEAD
+=======
+
+    user_data = <<-EOF
+                #!/bin.bash
+                sudo yum install -y nginx
+                sudo systemctl enable nginx
+                sudo syttemctl start nginx
+                EOF
+
+>>>>>>> develop
 }
