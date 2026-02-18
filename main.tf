0905b4d1c4f7e2c222e1d91a2b2c983531b907f6
Franklyn Duran <frdsoto.25@gmail.com>
2026-02-18 10:16:00 -0400

    Preparacion praa subir a GH

--- main.tf
+++ main.tf
@@ -6,7 +6,10 @@ resource "aws_instance" "nginx-server"{
     ami             = "ami-05efc83cb5512477c"
     instance_type   = "t3.micro"
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> release/v.3.0
 
     user_data = <<-EOF
                 #!/bin.bash
@@ -14,6 +17,44 @@ resource "aws_instance" "nginx-server"{
                 sudo systemctl enable nginx
                 sudo syttemctl start nginx
                 EOF
+<<<<<<< HEAD
 
 >>>>>>> develop
+=======
+key_name =aws_key_pair.nginex-server-ssh.key_name
+vpc_security_group_ids = [aws_security_group.nginex-server-sg.id]
+}
+
+resource "aws_key_pair" "nginex-server-ssh"{
+    key_name    = "nginex-server"
+    public_key  = file("nginex-server.pub")
+
+}
+
+resource "aws_security_group" "nginex-server-sg"{
+    name        = "nginex-server"
+    description = "Allow SSH and HTTP"
+
+    ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+        from_port   = 80
+        to_port     = 80
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress{
+        from_port   = 0
+        to_port     = 0
+        protocol    = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+
+    }
+>>>>>>> release/v.3.0
 }
